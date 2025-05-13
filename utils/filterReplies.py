8a3135fd202033e7392b0bc93bff4a3dc0303d19
Alexander Kleinwächter <alexander.kleinwaechter@stud.uni-bamberg.de>
2025-05-13 11:33:01 +0200

    Renaming

--- utils/filterReplies.py
+++ utils/filterReplies.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 tweets_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'twitter', 'tweets.csv')
-tweets_cleaned = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'twitter', 'tweets_cleaned.csv')
+tweets_cleaned = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data', 'twitter', 'tweets_isTweet.csv')
 tweets_path = os.path.abspath(tweets_path)
 tweets_cleaned = os.path.abspath(tweets_cleaned)
 
